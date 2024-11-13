dd0928134bac5a56eea13d0ced9c9038db02aedb
evans ongaloh <omondiongaloh@gmail.com>
2024-11-13 21:45:54 +0300

    react css

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,22 +1,12 @@
-import UserProfile from "./components/UserProfile";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import WelcomeMessage from "./components/WelcomeMessage.jsx";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// src/App.jsx
+import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
-      <WelcomeMessage />
-
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Header />
-      <MainContent />
-      <Footer />
+      <h1 style={{ textAlign: "center" }}>Counter Application</h1>
+      <Counter />
     </div>
   );
 }
