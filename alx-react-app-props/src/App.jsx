c420db83d04402309448cec40b47726d5c74aee8
evans ongaloh <omondiongaloh@gmail.com>
2024-11-14 20:37:26 +0300

    props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from "react";
 import ProfilePage from "./ProfilePage";
-import UserContext from "./UserContext";
+import UserContext from "./components/UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
