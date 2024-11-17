80760457832feef6da8a605410dc3dde23d16c32
evans ongaloh <omondiongaloh@gmail.com>
2024-11-17 22:20:46 +0300

    recipe sharing app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar";
+import AddRecipeForm from "./components/AddRecipeForm"; // Import AddRecipeForm
 
 function App() {
   return (
     <Router>
-      <div>
+      <div style={{ padding: "20px" }}>
+        <h1>Recipe Sharing Application</h1>
         <SearchBar />
+        <AddRecipeForm /> {/* Include AddRecipeForm */}
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipes/:id" element={<RecipeDetails />} />
