182031b85f6858f514dafe3b8446114b63aad85a
pazii-boop <omondiongaloh@gmail.com>
2024-11-30 15:56:16 +0300

    React Implementing tailwind-react-integration

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
+      <p className="mt-4 text-gray-700">Your Tailwind setup is complete!</p>
+    </div>
+  );
+}
+
+export default App;
