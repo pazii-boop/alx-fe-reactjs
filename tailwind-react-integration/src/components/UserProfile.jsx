7b18bb96ac2dc152823babc977825e44a322071a
pazii-boop <omondiongaloh@gmail.com>
2024-11-30 16:12:51 +0300

    React Implementing  Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,13 @@
 function UserProfile() {
   return (
-    <div
-      className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 
-      rounded-lg shadow-lg sm:p4 md:p8 sm:max-w-xs md:max-w-sm"
-    >
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:h-36 md:w-36"
         src="https://via.placeholder.com/150"
         alt="User"
+        className="w-36 h-36 rounded-full mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
-        Omondi Ongaloh
-      </h1>
-      <p className="text-gray-600 text-base sm:text-sm md:text-base">
+      <h1 className="text-xl text-blue-800 my-4 text-center">EVANS ONGALOH</h1>
+      <p className="text-gray-600 text-base text-center">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
