0e5a34f1660ba3a294c69b1de3268c94ac2b1725
pazii-boop <omondiongaloh@gmail.com>
2024-11-30 16:07:14 +0300

    React Implementing  Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
+import UserProfile from "./components/UserProfile";
+import "./App.css";
+
 function App() {
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
-      <p className="mt-4 text-gray-700">Your Tailwind setup is complete!</p>
-    </div>
+    <>
+      <UserProfile />
+    </>
   );
 }
 
