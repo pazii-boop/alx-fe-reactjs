f86485c7ca1f62ed7e08206424311fb2e1c5f4c0
pazii-boop <omondiongaloh@gmail.com>
2024-12-15 18:43:38 +0300

    github-user-search

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -41,7 +41,7 @@ const Search = () => {
 
       {/* Conditional Rendering */}
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user.</p>}
+      {error && <p>Looks like we can't find the user</p>}
       {userData && (
         <div>
           <img src={userData.avatar_url} alt={userData.login} width={100} />
